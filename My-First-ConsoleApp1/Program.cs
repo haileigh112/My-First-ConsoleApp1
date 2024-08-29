144d188c88c670970de99b3d5fd58cfc3c37c0a1
Haileigh Stanton <haileigh112@gmail.com>
2024-08-29 19:18:47 -0400

    first commit with gitignore

---
+++ My-First-ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+var CurrentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine} Hello, {name}, on {CurrentDate:d}");
+Console.Write($"{Environment.NewLine} Press any key to exit...");
+Console.ReadKey(true);
